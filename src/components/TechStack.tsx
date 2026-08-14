725267cbe38db5a46de4ba85a28397785ccae175
Siddharth_Hiraou <89652594+SiddharthHiraou@users.noreply.github.com>
2026-08-14 01:44:35 -0400

    Update tech stack icons to AI/ML tools

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,14 +13,16 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/python.svg",
+  "/images/pytorch.svg",
+  "/images/tensorflow.svg",
+  "/images/docker.svg",
+  "/images/kubernetes.svg",
+  "/images/aws.svg",
+  "/images/gcp.svg",
+  "/images/git.svg",
+  "/images/java.svg",
+  "/images/postgres.svg",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
